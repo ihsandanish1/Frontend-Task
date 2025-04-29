676bb8c37d800a33aaec86fb6b040cebf405021e
email <beaprogrammer240gmail.com>
2025-04-29 13:23:38 +0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Restricted from './Restricted'
+import ChampionSilver from './pages/ChampionSliver'
 
 function App() {
   return (
-    <>
-      <Restricted/>
-    </>
+    <Routes>
+      <Route path="/" element={<Restricted />} />
+      <Route path="/champion-icy" element={<ChampionSilver />} />
+    </Routes>
   )
 }
 
 export default App
+
